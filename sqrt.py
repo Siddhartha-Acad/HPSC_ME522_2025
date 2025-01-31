459acaf002b8197b6c76db9c4a0b8ddff0af21ae
Siddhartha-Acad <siddharthamithiya.acad2001@gmail.com>
2025-01-31 11:13:52 +0530

    Added few comments for clarity.

--- sqrt.py
+++ sqrt.py
@@ -1,7 +1,7 @@
-x0 = 1.0
-s = 2.0
+x0 = 1.0	# initial guess of the root.
+s = 2.0		# number of which square root to be calculated.
 
-for i in range(20):
-	x0 = 0.5 * (x0 + s/x0)
-	print(f'{i=}: {x0=}')
+for i in range(20):				# starting the loop.
+	x0 = 0.5 * (x0 + s/x0)		# updating the value of root.
+	print(f'{i=}: {x0=}')		# printing info.
 
